4b2b0de6b8e05777edff21a4523bd5ec2fcc24bd
CKHarrison <hirohumi1882@gmail.com>
2020-04-21 20:45:52 -0700

    Initalize repository

---
+++ lib/ckh_palindrome.rb
@@ -0,0 +1,6 @@
+require "ckh_palindrome/version"
+
+module CkhPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ckh_palindrome/version.rb
@@ -0,0 +1,3 @@
+module CkhPalindrome
+  VERSION = "0.1.0"
+end
